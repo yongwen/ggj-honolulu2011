f431a23b620198e1d6d591da40df169e45c7ddb3
DavidNChin@gmail.com <DavidNChin@gmail.com@90b5d554-e423-72d7-c178-64feaf65eccf>
2011-01-30 04:30:15 +0000

    Moves roaches toward closest food.

---
+++ trunk/src/edu/hawaii/ics/roach/enemylogic/GoTowardFood.java
@@ -0,0 +1,145 @@
+package edu.hawaii.ics.roach.enemylogic;
+
+// JFC
+import java.awt.image.BufferedImage;
+import java.math.*;
+//GTGE
+import com.golden.gamedev.object.Sprite;
+import com.golden.gamedev.object.SpriteGroup;
+import com.golden.gamedev.object.Timer;
+import com.golden.gamedev.util.Utility;
+//ROACH
+import edu.hawaii.ics.roach.Enemy;
+import edu.hawaii.ics.roach.Roach;
+import edu.hawaii.ics.roach.RoachSprite;
+
+public class GoTowardFood extends Enemy {
+
+	private Roach	game;
+	private Timer 	timer;
+
+	public GoTowardFood(Roach game,
+						   BufferedImage[] image, double x, double y,
+				 		   double speed, long animationDelay) {
+		super(game,image,x,y,speed,animationDelay);
+		this.game = game;
+		
+		setAnimate(true);
+		setLoopAnim(true);
+
+		timer = new Timer(10);
+
+		moveToFood(-1);
+	}
+	
+	private void moveToFood(int disallowDir) {
+		int dir = -1;
+		Sprite closestFood = findClosestFood();
+		System.out.println("roach "+this+" is at ("+this.getX()+", "+this.getY()+")");
+		
+		if(closestFood == null) {
+			// no more food?
+			System.out.println("no more food?");
+			changeDirection(disallowDir);
+			timer.setDelay(2000);
+			return;
+		}
+		double horizDistance = this.getX() - closestFood.getX();
+		double vertDistance = this.getY() - closestFood.getY();
+		System.out.println("vertDistance = "+vertDistance);
+		System.out.println("horizDistance = "+horizDistance);
+		if(Math.abs(vertDistance) > Math.abs(horizDistance)) {
+			if(vertDistance > 0) {
+				if(disallowDir != UP) { System.out.println("moving UP"); setDirection(UP); return; }
+				if(horizDistance > 0) setDirection(LEFT);
+				else setDirection(RIGHT);
+				return;
+			} else { // vertDistance <= 0
+				if(disallowDir != DOWN) { System.out.println("moving DOWN"); setDirection(DOWN); return; }
+				if(horizDistance > 0) setDirection(LEFT);
+				else setDirection(RIGHT);
+				return;
+			}
+		} else { // Math.abs(vertDistance) <= Math.abs(horizDistance)
+			if(horizDistance > 0) {
+				if(disallowDir != LEFT) { System.out.println("moving LEFT"); setDirection(LEFT); return; }
+				if(vertDistance > 0) setDirection(UP);
+				else setDirection(DOWN);
+				return;
+			} else { //horizDistance <= 0
+				if(disallowDir != RIGHT) { System.out.println("moving RIGHT"); setDirection(RIGHT); return; }
+				if(vertDistance > 0) setDirection(UP);
+				else setDirection(DOWN);
+				return;
+			}
+		}
+	}
+
+	private void changeDirection(int disallowDir) {
+		int dir = Utility.getRandom(LEFT, DOWN);
+		while (dir == disallowDir) {
+			dir = Utility.getRandom(LEFT, DOWN);
+		}
+
+		setDirection(dir);
+	}
+	
+	public Boolean isActiveFood(Sprite sprite) {
+		if(sprite == null) return false;
+		Boolean isActive = sprite.isActive();
+		int id = sprite.getID();
+		return (sprite != null && sprite.isActive() && sprite.getID() >= 1 && sprite.getID() <= 5);
+	}
+	
+	private Sprite findClosestFood() {
+		Sprite[] allFood = game.allFood().getSprites();
+		Sprite closestFood = null;
+		double closestDistance = 10000000000.0;
+		for (Sprite sprite : allFood) {
+			if(isActiveFood(sprite)) {
+				double distance = Math.pow(this.getX()-sprite.getX(), 2) + Math.pow(this.getY()-sprite.getY(), 2);
+				if(distance<closestDistance) {
+					closestDistance = distance;
+					closestFood = sprite;
+				}
+			}
+			
+		}
+		if(closestFood == null) System.out.println("no more food!");
+		else System.out.println("closest food is "+closestFood+" at ("+closestFood.getX()+", "+closestFood.getY()+")");
+		return closestFood;
+	}
+	
+	public void update(long elapsedTime) {
+		if (timer.action(elapsedTime)) {
+			moveToFood(-1);
+
+			timer.setDelay(10);
+		}
+
+		super.update(elapsedTime);
+	}
+	
+	public void hitWall() {
+		super.hitWall();
+
+		// target direction and current direction must be different
+		moveToFood(getDirection());
+
+		timer.setDelay(300);
+	}
+	
+	private Sprite findLeastTargetedFood() {
+		Sprite[] allFood = game.allFood().getSprites();
+		Sprite closestFood = null;
+		double closestDistanceSquared = 10000000.0;
+		for (Sprite sprite : allFood) {
+			double distanceSquared = Math.pow(this.getX()-sprite.getX(), 2) + Math.pow(this.getY()-sprite.getY(), 2);
+			if(distanceSquared<closestDistanceSquared) {
+				closestDistanceSquared = distanceSquared;
+				closestFood = sprite;
+			}
+		}
+		return closestFood;
+	}
+}
