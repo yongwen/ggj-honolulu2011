1a2e26d0fcbc1796441baaf33af2946534301fa8
DavidNChin@gmail.com <DavidNChin@gmail.com@90b5d554-e423-72d7-c178-64feaf65eccf>
2011-01-30 02:21:24 +0000

    added method to get food sprites

--- src/edu/hawaii/ics/roach/Roach.java
+++ src/edu/hawaii/ics/roach/Roach.java
@@ -274,17 +274,17 @@ public class Roach extends GameObject implements Comparator {
 					// stand still
 					case 0: enemy = new Enemy(this, image, posx, posy, speed, animationDelay); break;
 					// left-right patrol
-					case 1: enemy = new LeftRightPatrol(this, image, posx, posy, speed, animationDelay); break;
+					case 1: enemy = new GoTowardFood(this, image, posx, posy, speed, animationDelay); break;
 					// up-down patrol
-					case 2: enemy = new UpDownPatrol(this, image, posx, posy, speed, animationDelay); break;
+					case 2: enemy = new GoTowardFood(this, image, posx, posy, speed, animationDelay); break;
 					// turn left always
-					case 3: enemy = new TurnLeftAlways(this, image, posx, posy, speed, animationDelay); break;
+					case 3: enemy = new GoTowardFood(this, image, posx, posy, speed, animationDelay); break;
 					// turn right always
-					case 4: enemy = new TurnRightAlways(this, image, posx, posy, speed, animationDelay); break;
+					case 4: enemy = new GoTowardFood(this, image, posx, posy, speed, animationDelay); break;
 					// turner
-					case 5: enemy = new Turner(this, image, posx, posy, speed, animationDelay); break;
+					case 5: enemy = new GoTowardFood(this, image, posx, posy, speed, animationDelay); break;
 					// zombie
-					case 6: enemy = new Zombie(this, image, posx, posy, speed, animationDelay, player); break;
+					case 6: enemy = new GoTowardFood(this, image, posx, posy, speed, animationDelay); break;
 				}
 				if (enemy != null) {
 					ENEMY_GROUP.add(enemy);
@@ -836,6 +836,10 @@ public class Roach extends GameObject implements Comparator {
 			case 36: return true; 	// floor
 			default: return false;	// wall
 		}
+	}
+	
+	public SpriteGroup allFood() {
+		return UPPER_GROUP;
 	}
 
 
