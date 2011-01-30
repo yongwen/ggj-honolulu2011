00fd4de035746734a72f8f9f8b7b67a302630de9
DavidNChin@gmail.com <DavidNChin@gmail.com@90b5d554-e423-72d7-c178-64feaf65eccf>
2011-01-30 07:58:37 +0000

    Corrected credits.

--- src/edu/hawaii/ics/roach/menu/Credits.java
+++ src/edu/hawaii/ics/roach/menu/Credits.java
@@ -61,11 +61,17 @@ public class Credits extends GameObject {
 		g.fillRect(0, 0, getWidth(), getHeight());
 		g.drawImage(titleImage, 10, 10, null);
 
-		font.drawString(g, "Original Game By:", GameFont.CENTER, 0, 125, getWidth());
-		g.drawImage(creditsImage, 120, 160, null);
-
-		font.drawString(g, "Remake By", GameFont.CENTER, 0, 380, getWidth());
-		font.drawString(g, "PAULUS TUERAH", GameFont.CENTER, 0, 414, getWidth());
+		
+		font.drawString(g, "Written in 48 hrs", GameFont.CENTER, 0, 175, getWidth());
+		font.drawString(g, "for Global Game Jam 2011", GameFont.CENTER, 0, 200, getWidth());
+		font.drawString(g, "Based on the Warlock game", GameFont.CENTER, 0, 250, getWidth());
+		font.drawString(g, "by Paulus Tuerah", GameFont.CENTER, 0, 275, getWidth());
+		
+		font.drawString(g, "Authors:", GameFont.CENTER, 0, 325, getWidth());
+		font.drawString(g, "David Chin", GameFont.CENTER, 0, 350, getWidth());
+		font.drawString(g, "Gorm Lai", GameFont.CENTER, 0, 375, getWidth());
+		font.drawString(g, "Robert Puckett", GameFont.CENTER, 0, 400, getWidth());
+		font.drawString(g, "Yongwen Xu", GameFont.CENTER, 0, 425, getWidth());
 	}
 
 }
